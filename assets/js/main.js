8756a931bd4bb234a5d70de50b089dbe9a19fed0
Igor dos Santos <santosigor013@gmail.com>
2018-08-09 16:11:47 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 // Ancora
-
 $('.acr').on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 50}, 800, 'linear');
+});
+
+// Proximo passo
+$('.btn-next').on('click', function (e) {
+  e.preventDefault();
+  $(this).hide();
+  $('.acr').click();
+  $('#passo2').css('display','inline-block').addClass('animation-passo2');
+  $('.btn-finish').css('display','inline-block');
 });
